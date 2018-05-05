7d4d29c5d5c83c8c0b29793e7514ded4cc1f4bc7
rnitta <attinyes@gmail.com>
2018-05-05 21:30:38 +0900

    ルーティング整理

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
   root to: 'mock#top'
+  resources :users, only: %i[show]
+  resources :categories, only: %i[new create show edit update destroy index]
+  resources :favorites, only: %i[create destroy]
+  resources :subjects, only: %i[new create show edit update destroy index] do
+    resources :manners, only: %i[new create show edit update destroy index]
+  end
   devise_for :users, controllers:  {
     # deviseのコントローラをオーバーライドしてるやつを呼ぶ設定
     registrations: 'users/registrations',
@@ -8,12 +14,7 @@ Rails.application.routes.draw do
     confirmations: 'users/confirmations'
   }
   get 'mypage', to: 'users#mypage'
-  resources :users, only: %i[show]
-  resources :subjects, only: %i[new create show edit update destroy index] do
-    resources :manners, only: %i[new create show edit update destroy index]
-  end
   get 'ogp', to: 'ogp#tw_subject'
-  resources :categories, only: %i[new create show edit update destroy index]
-  get 'mock/top', to: 'mock#top'
   get 'sitemap', to: 'static_pages#sitemap'
+  get 'mock/top', to: 'mock#top'
 end
