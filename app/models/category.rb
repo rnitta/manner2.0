62069d671db696caf31e995ab2802bf5f9221f30
rnitta <attinyes@gmail.com>
2018-05-18 09:03:56 +0900

    category_indexページ作った

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal:true
-
 # == Schema Information
 #
 # Table name: categories
 #
-#  id          :integer          not null, primary key
-#  name        :string           not null
-#  friendly_id :string           not null
-#  count       :integer          default(0), not null
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id             :integer          not null, primary key
+#  name           :string           not null
+#  friendly_id    :string           not null
+#  count          :integer          default(0), not null
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  subjects_count :integer          default(0), not null
 #
 
 class Category < ApplicationRecord
