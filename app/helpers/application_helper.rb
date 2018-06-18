6841ac48da0abe55cbe8c1d8874e842cb4f0746f
rnitta <attinyes@gmail.com>
2018-06-18 09:49:36 +0900

    no message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,10 +14,27 @@ module ApplicationHelper
       keywords: Settings.site.page_keywords,
       canonical: request.base_url + request.path,
       og: {
-        title: :title,
+        title: Settings.site.page_title,
         type: Settings.site.meta.ogp.type,
         url: request.original_url,
         image: request.base_url + Settings.site.meta.ogp.image_path,
+        site_name: Settings.site.name,
+        description: :description,
+        locale: 'ja_JP'
+      },
+      twitter: {
+        card: 'summarylargeimage'
+      }
+    }
+  end
+
+  def subject_meta_tags(subject)
+    {
+      og: {
+        title: "#{subject.title}を教えてください。 | #{Settings.site.name}",
+        type: Settings.site.meta.ogp.type,
+        url: subject_url(subject),
+        image: ogp_url(subject.id),
         site_name: Settings.site.name,
         description: :description,
         locale: 'ja_JP'
